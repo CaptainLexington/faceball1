c06da40e70f5c2130afc928bdec6d8a5b3949c2b
Matt <loggerisms@gmail.com>
2014-01-25 15:46:51 -0600

    Something having to do with lighting?

--- scene.js
+++ scene.js
@@ -1,12 +1,12 @@
 function initializeScene(scene){
 	scene.gravity = new BABYLON.Vector3(0,-9.81,0);	
 	scene.collisionsEnabled=true;
-	var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0,0,0), scene);
+	var camera = new BABYLON.FreeCamera("Camera", new BABYLON.Vector3(0,20,0), scene);
 	camera.ellipsoid = new BABYLON.Vector3(1,1,1);
-	camera.applyGravity=true;
+	//camera.applyGravity=true;
 	scene.activeCamera = camera;
 	camera.checkCollisions=true;
-	var light0 = new BABYLON.DirectionalLight("Dir0", new BABYLON.Vector3(0,-1,0), scene);
+	var light0 = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0,-1,0), scene);
 	window.camera = camera;
 	var people = new Array();
 	for (var i=0; i<5; i++){
