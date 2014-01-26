9acc838c22de5671b11a70a86e23f02c43f9fa96
C. Warren Dale <adventureonthehighseas@gmail.com>
2014-01-26 13:42:21 -0600

    Faces

--- line.js
+++ line.js
@@ -10,58 +10,58 @@
 *****Cho Bunbo
 *****Chorming Feebus
 *****Lazbon
-****/
+*****/
 
-var monolgoues = 
-	[{line: "Here I am at the party!",
-	  sadness: 1,
-	  anger: 0,
-          joy: 1,
-          anxiety: 2}];
+var monolgoues =
+    [{line: "Here I am at the party!",
+      sadness: 1,
+      anger: 0,
+      joy: 1,
+      anxiety: 2}];
 
 var TwelfDialogue =
-	{ "terliggidy" :
-	  [{line: "Some weather we're having, eh?"}],
-	  "wumpy" :
-	  [{line: "Uh...yeah." },
-	   {line: "I can see why you would think that, but no."}],
-	  "Snoopler" :
-	  [{line: "Um, what?"}],
-	  "Lepsis" : 
-	  [{line: "Hey, uh, you left me a voicemail?"},
-	   {line: "So what did it say?"},
-	   {line: "Well, no."},
-	   {line: "You're here now.Just tell me!"},]
-        };
-?
+    { "terliggidy" :
+     [{line: "Some weather we're having, eh?"}],
+     "wumpy" :
+     [{line: "Uh...yeah." },
+      {line: "I can see why you would think that, but no."}],
+     "Snoopler" :
+     [{line: "Um, what?"}],
+     "Lepsis" :
+     [{line: "Hey, uh, you left me a voicemail?"},
+      {line: "So what did it say?"},
+      {line: "Well, no."},
+      {line: "You're here now.Just tell me!"}]
+    };
+
 var WunkDialogue =
-	[{line: ""}];
+    [{line: ""}];
 
 var TerliggidyDialogue =
-	[{line: "I wouldn't say so, no"}];
+    [{line: "I wouldn't say so, no"}];
 
 var WumpyDialogue =
-	[{line: "Is the Simpsons still on?"},
-	 {line: "Oh good."];
+    [{line: "Is the Simpsons still on?"},
+     {line: "Oh good."}];
 
 var SnooplerDialogue =
-	[{line: "Mickey Mouse denies allegations he's seeking a divorce from Minnie on grounds of mental instability."},
-	 {line: "All he said was that she was fucking Goofy."}];
+    [{line: "Mickey Mouse denies allegations he's seeking a divorce from Minnie on grounds of mental instability."},
+     {line: "All he said was that she was fucking Goofy."}];
 
 var LepsisDialogue =
-	[{line: "Yeah."},
-         {line: "What, you didn't listen to it?"},
-         {line: "Why not?"},
-         {line: "*sigh*"},];
+    [{line: "Yeah."},
+     {line: "What, you didn't listen to it?"},
+     {line: "Why not?"},
+     {line: "*sigh*"}];
 
 var KeleryDialogue =
-	[{line: ""}];
+    [{line: "This is so dumb. I could be in Madison right now. Why didn't I go to Madison?"}];
 
 var ChoBunboDialogue =
-	[{line: ""}];
+    [{line: "I've had a burrito for breakfast every day this week. My gas station has so many flavors it's crazy."}];
 
 var ChormingDialogue =
-	[{line: ""}];
+    [{line: "We need more booze. There level of booze here is insufficient for my current needs."}];
 
 var LazbonDialogue =
-	[{line: ""}];
+    [{line: "It must be a nightmare for Snoopler and Whumpy to try to get ready in the morning with only one bathroom and three sets of toothbrushes."}];
